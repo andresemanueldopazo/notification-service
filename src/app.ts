717f78e9d3f162198541e931091cc33956301d5d
ignaciodopazo <ignacio.dopazo11@gmail.com>
2023-03-08 23:09:08 -0300

    feat: add helmet middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
+import helmet from "helmet";
 
 const app = express();
+
+app.use(helmet());
 
 type Parent = {
   name: string

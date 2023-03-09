c84b256cf774b4475da1c2c94c34301985422e8e
ignaciodopazo <ignacio.dopazo11@gmail.com>
2023-03-08 22:42:56 -0300

    feat: shared array between route handlers

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+type Parent = {
+  name: string
+  response: express.Response
+};
+
+const parents: Array<Parent> = [];
+
+// Returns the connected parents
+app.get("/connected", (req, res) => {
+  res.send(parents.map((p) => p.name));
+});
+
+// Add the parent to the connected list of parents
+app.post("/connect", bodyParser.text(), (req, res) => {
+  const parentName = req.body;
+  parents.push({ name: parentName, response: res });
+  res.send(`${parentName} connected!`);
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}!`);
+});
